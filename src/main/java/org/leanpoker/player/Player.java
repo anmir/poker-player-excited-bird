81d26c243e88921bef79f958c087e8a5f9f8f287
Andrey.Miroshnikov <Andrey.Miroshnikov@billing.ru>
2017-08-12 10:59:04 +0300

    random(500)

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,14 +2,15 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-import java.util.Map;
+import java.util.Random;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
+    static private Random random = new Random();
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        return random.nextInt(500);
     }
 
     public static void showdown(JsonElement game) {
