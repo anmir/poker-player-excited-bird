39156ead7293afd3cfa09e62d4f674a11029b36e
Andrey.Miroshnikov <Andrey.Miroshnikov@billing.ru>
2017-08-12 12:44:54 +0300

    constants added

---
+++ src/main/java/org/leanpoker/player/constants/CardRanks.java
@@ -0,0 +1,20 @@
+package org.leanpoker.player.constants;
+
+/**
+ * Created by andrey on 12.08.17.
+ */
+public class CardRanks {
+    public Integer _2 = 2;
+    public Integer _3 = 3;
+    public Integer _4 = 4;
+    public Integer _5 = 5;
+    public Integer _6 = 6;
+    public Integer _7 = 7;
+    public Integer _8 = 8;
+    public Integer _9 = 9;
+    public Integer _10 = 10;
+    public Integer _J = 11;
+    public Integer _Q = 12;
+    public Integer _K = 13;
+    public Integer _A = 14;
+}
---
+++ src/main/java/org/leanpoker/player/constants/CardSuits.java
@@ -0,0 +1,11 @@
+package org.leanpoker.player.constants;
+
+/**
+ * Created by andrey on 12.08.17.
+ */
+public class CardSuits {
+    private String HEARTS = "hearts";
+    private String SPADES = "spades"; // пики
+    private String DIAMONDS = "diamonds";// буби
+    private String CLUBS = "clubs"; // крести
+}
