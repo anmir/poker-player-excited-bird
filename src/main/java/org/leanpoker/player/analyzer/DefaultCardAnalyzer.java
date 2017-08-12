37f54e41da1126b109dcf517090d2e50a8bbe218
Andrey.Miroshnikov <Andrey.Miroshnikov@billing.ru>
2017-08-12 14:39:13 +0300

    double_pair

--- src/main/java/org/leanpoker/player/analyzer/DefaultCardAnalyzer.java
+++ src/main/java/org/leanpoker/player/analyzer/DefaultCardAnalyzer.java
@@ -3,6 +3,7 @@ package org.leanpoker.player.analyzer;
 import org.leanpoker.player.Card;
 import org.leanpoker.player.constants.Combination;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,10 +26,37 @@ public class DefaultCardAnalyzer implements CardAnalyzer {
                 return new CardAnalyzeResult(Combination.TRIPLE, biggestTripleRank);
             }
 
+            Integer biggestDoublePairRank = isDoublePair(cards);
+            if (biggestDoublePairRank != null) {
+                return new CardAnalyzeResult(Combination.DOUBLE_PAIR, biggestDoublePairRank);
+            }
+
         }
         Card biggestCard = Collections.max(cards);
         return new CardAnalyzeResult(Combination.BIGGEST_CARD, biggestCard.getRank().getOrdr());
     }
+
+
+    public static Integer isDoublePair(List<Card> cards) {
+
+        Integer biggestCardInFirstPair = isPair(cards);
+        if (biggestCardInFirstPair != null) {
+            // удаляем текущие, ищем еще пару
+            List<Card> cardsWithoutFirstPair = new ArrayList<>(cards);
+            for (int i = 0; i < cardsWithoutFirstPair.size(); i++) {
+                Card card = cardsWithoutFirstPair.get(i);
+                if (card.getRank().getOrdr().equals(biggestCardInFirstPair)) {
+                    cardsWithoutFirstPair.remove(card);
+                }
+            }
+            Integer biggestCardInSecondPair = isPair(cardsWithoutFirstPair);
+            if (biggestCardInSecondPair != null) {
+                return biggestCardInSecondPair;
+            }
+        }
+        return null;
+    }
+
 
     public static Integer isPair(List<Card> cards) {
         for (int i = 0; i < cards.size(); i++) {
--- src/test/java/org/leanpoker/player/analyzer/DefaultCardAnalyzerTest.java
+++ src/test/java/org/leanpoker/player/analyzer/DefaultCardAnalyzerTest.java
@@ -32,4 +32,23 @@ public class DefaultCardAnalyzerTest {
         Integer biggestTripleCard = DefaultCardAnalyzer.isTriple(cards);
         Assert.assertEquals(new Integer(2), biggestTripleCard);
     }
+
+    @Test
+    public void isDoublePair() throws Exception {
+        List<Card> cards = new ArrayList<>();
+        Card card1 = new Card("2", CardSuits.CLUBS.getVal());
+        Card card2 = new Card("2", CardSuits.DIAMONDS.getVal());
+        Card card3 = new Card("4", CardSuits.CLUBS.getVal());
+        Card card4 = new Card("8", CardSuits.SPADES.getVal());
+        Card card5 = new Card("4", CardSuits.DIAMONDS.getVal());
+
+        cards.add(card1);
+        cards.add(card2);
+        cards.add(card3);
+        cards.add(card4);
+        cards.add(card5);
+
+        Integer biggestTripleCard = DefaultCardAnalyzer.isDoublePair(cards);
+        Assert.assertEquals(new Integer(4), biggestTripleCard);
+    }
 }
