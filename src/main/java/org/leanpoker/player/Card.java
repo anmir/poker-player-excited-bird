b264981925c4855d573e5c0e2725e0a6bd72abbc
Andrey.Miroshnikov <Andrey.Miroshnikov@billing.ru>
2017-08-12 11:51:30 +0300

    pair strategy

---
+++ src/main/java/org/leanpoker/player/Card.java
@@ -0,0 +1,51 @@
+package org.leanpoker.player;
+
+/**
+ * Created by andrey on 12.08.17.
+ */
+public class Card {
+    private String rank;
+    private String suit;
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (rank != null ? !rank.equals(card.rank) : card.rank != null) return false;
+        return suit != null ? suit.equals(card.suit) : card.suit == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = rank != null ? rank.hashCode() : 0;
+        result = 31 * result + (suit != null ? suit.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "rank='" + rank + '\'' +
+                ", suit='" + suit + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/leanpoker/player/PairStrategy.java
@@ -0,0 +1,14 @@
+package org.leanpoker.player;
+
+import java.util.List;
+
+/**
+ * Created by andrey on 12.08.17.
+ */
+public class PairStrategy {
+
+    public int process(List<Card> cards) {
+        return 1;
+    }
+
+}
